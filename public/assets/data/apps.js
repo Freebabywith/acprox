bf2ce073942f02c1db7e0ff2a8c9c91e02a53d0c
Freebabywith <131982517+Freebabywith@users.noreply.github.com>
2024-06-11 20:58:40 -0500

    Update apps.js

--- public/assets/data/apps.js
+++ public/assets/data/apps.js
@@ -5,22 +5,47 @@ var apps = [
       'image': 'assets/images/add.png'
     },
     {
-      "id": "google",
-      "title": "Google",
-      "url": "https://www.google.com/?safe=active&ssui=on",
-      'image': 'https://img.freepik.com/free-icon/search_318-265146.jpg'
-    },
-    {
       "id": "Rom Files",
-      "title" : "Rom Files",
+      "title": "Rom Files for Emulator",
       "url": "https://sites.google.com/site/minecrafterdiskstation/roms",
       'image': 'https://img.freepik.com/free-icon/search_318-265146.jpg'
+      
     },
     {
-      "id": "Purepro",
-      "title": "Purepro",
+      "id": "PurePro",
+      "title": "PurePro Games",
       "url": "https://dsa-games.vercel.app/",
-      'image': 'https://img.freepik.com/free-icon/search_318-265146.jpg'
+      'image': 'https://static.vecteezy.com/system/resources/previews/006/892/625/original/discord-logo-icon-editorial-free-vector.jpg'
+    },
+    {
+      "id": "telegram",
+      "title": "Telegram",
+      "url": "https://web.telegram.org/a/",
+      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png'
+    }, 
+    {
+      "id": "geforce",
+      "title": "GeForce NOW (may not work)",
+      "url": "https://play.geforcenow.com",
+      'image': 'https://raw.githubusercontent.com/proudparrot2/cdn/main/8z9zeDIT_400x400-removebg-preview.png'
+    },
+    {
+      'id': 'vscode',
+      'title': 'Visual Studio Code',
+      'url': 'https://vscode.dev/',
+      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png'
+    },
+    {
+      "id": "reddit",
+      "title": "Reddit",
+      "url": "https://reddit.com",
+      'image': 'https://www.redditinc.com/assets/images/site/reddit-logo.png'
+    },
+    {
+      "id": "youtube",
+      "title": "YouTube",
+      "url": "https://youtube.com",
+      'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/YouTube_social_red_square_%282017%29.svg/2048px-YouTube_social_red_square_%282017%29.svg.png'
     }
   ]
   
